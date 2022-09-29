58bf4eead521e4e6ed3984c90446d7c20bd82305
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-09-29 11:38:01 -0500

    Welcome Screen

--- App.js
+++ App.js
@@ -1,41 +1,19 @@
+import { View, StyleSheet } from 'react-native';
 import {
-  StyleSheet,
-  Button,
-  Alert,
-  Text,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from 'react-native';
+  useDimensions,
+  useDeviceOrientation,
+} from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        color="orange"
-        title="Juancho"
-        onPress={() =>
-          Alert.alert('Te gusta la sapada?', 'Confirma', [
-            { text: 'Simon', onPress: () => console.log('Simon') },
-            { text: 'Nola', onPress: () => console.log('Nola') },
-          ])
-        }
-      />
-      <Text style={styles.text}>Hi world</Text>
-    </SafeAreaView>
-  );
+  console.log(useDimensions(), useDeviceOrientation());
+  const { landscape } = useDeviceOrientation();
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-  text: {
-    color: 'tomato',
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require('../assets/logo-red.png')} style={styles.logo} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+});
+
+export default WelcomeScreen;

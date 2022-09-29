6b97d84a53d91708e8f6fd197b2f7d9baf981b13
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-09-29 11:55:45 -0500

    View image screen

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import {
   useDeviceOrientation,
 } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   console.log(useDimensions(), useDeviceOrientation());
   const { landscape } = useDeviceOrientation();
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({

49213c86f45b112ea7c7aac6d5b48477843d818d
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-09-30 11:17:55 -0500

    Welcome Screen with buttons

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import {
   useDimensions,
   useDeviceOrientation,
 } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
   console.log(useDimensions(), useDeviceOrientation());
   const { landscape } = useDeviceOrientation();
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       source={require('../assets/background.jpg')}
       style={styles.background}
     >
       <View style={styles.logoContainer}>
         <Image source={require('../assets/logo-red.png')} style={styles.logo} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -23,16 +27,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  loginButton: {
-    width: '100%',
-    height: 70,
-    backgroundColor: '#fc5c65',
-  },
-  registerButton: {
-    width: '100%',
-    height: 70,
-    backgroundColor: '#4ecdc4',
-  },
   logo: {
     width: 100,
     height: 100,
@@ -41,6 +35,15 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 70,
     alignItems: 'center',
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: '100%',
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: '600',
+    paddingVertical: 20,
   },
 });
 
